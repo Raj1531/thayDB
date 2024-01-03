d954f533d930425eadc26b5e22e4c8e6c6490e92
TheInvestorCo-Tech <130795027+TheInvestorCo-Tech@users.noreply.github.com>
2024-01-03 19:45:06 +0530

    DB

--- server.js
+++ server.js
@@ -1,49 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require("cors")
-require('express-async-errors'),
-bodyparser = require('body-parser');
-const db = require('./config/db');
-const employeeRoutes = require('./controllers/employeeController');
-const rolesRoutes = require('./controllers/roleController');
-const holidayRoutes = require('./controllers/holidayController');
-const timeRoutes=require('./controllers/timeController');
-const sendEmail = require('./contactUs');
+const cors = require("cors");
+require("express-async-errors"), (bodyparser = require("body-parser"));
+const db = require("./config/db");
+const employeeRoutes = require("./controllers/employeeController");
+const rolesRoutes = require("./controllers/roleController");
+const holidayRoutes = require("./controllers/holidayController");
+const timeRoutes = require("./controllers/timeController");
+const sendEmail = require("./contactUs");
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/employee', employeeRoutes);
-app.use((err,req,res,next) => {
-  console.log(err)
-  res.status(err.status || 500).send('Something went wrong!')
-})
-
-//middleware
-app.use(cors())
-app.use(express.json())
-app.use('/api/roles', rolesRoutes);
-app.use((err,req,res,next) => {
-  console.log(err)
-  res.status(err.status || 500).send('Something went wrong!')
-})
+app.use(cors());
+app.use(express.json());
+app.use("/api/employee", employeeRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
+});
 
 //middleware
-app.use(cors())
-app.use(express.json())
-app.use('/api/holiday', holidayRoutes);
-app.use((err,req,res,next) => {
-  console.log(err)
-  res.status(err.status || 500).send('Something went wrong!')
-})
+app.use(cors());
+app.use(express.json());
+app.use("/api/roles", rolesRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
+});
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/time',timeRoutes);
-app.use((err,req,res,next) => {
-  console.log(err)
-  res.status(err.status || 500).send('Something went wrong!')
-})
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use("/api/holiday", holidayRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
+});
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/time", timeRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
+});
 
 app.get("/email", (req, res) => {
   sendEmail(req.query)
@@ -52,7 +50,9 @@ app.get("/email", (req, res) => {
 });
 
 db.query("SELECT 1")
-.then(() => {console.log('db connection succeeded')
-app.listen(5000,
-  () => console.log ('server started at 5000'))})
-.catch(err => console.log('db connection failed. \n' + err))
+  .then(() => {
+    console.log("db connection succeeded");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`server started at ${PORT}`));
+  })
+  .catch((err) => console.log(`db connection failed at ${PORT}.\n ${err}`));
