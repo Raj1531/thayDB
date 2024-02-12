c0202b25e3356f623eaf0908c09e811ecc80b9e4
TheInvestorCo-Tech <130795027+TheInvestorCo-Tech@users.noreply.github.com>
2024-02-12 13:26:00 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise')
 
 const mysqlPool = mysql.createPool({
   host:'sql6.freesqldatabase.com',
-  user:'sql6681328',
-  password:'LzkGlElYIZ',
-  database:'sql6681328',
+  user:'sql6683525',
+  password:'Ar72pcHStF',
+  database:'sql6683525',
   
 })
 
